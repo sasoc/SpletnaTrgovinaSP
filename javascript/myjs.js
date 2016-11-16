50a8464d2393b9327c34e85bf6ee28f88ef88477
sasoc <sc5879@student.uni-lj.si>
2016-11-16 12:39:43 +0100

    Update main tag

--- javascript/myjs.js
+++ javascript/myjs.js
@@ -2,8 +2,43 @@
 
 window.onload = function(){ 
     document.getElementById("aside_button").onclick = function() {expand()};
+   
+    
+    
 };
 
+document.addEventListener("DOMContentLoaded", function(event){
+  // your code here
+  for (var i = 0; i < 10; i++) {
+
+  	var img=document.createElement("div");
+    /*img.setAttribute("src", "images/defaultPL.png");*/
+
+    if(i==2){
+    	img.style.backgroundImage = "url('images/devils.jpg')";
+    }else if(i==3){
+    	img.style.backgroundImage = "url('images/pink-floyd-dark-side-of-the-moon-album-cover.jpg')";
+    }else {
+    	img.style.backgroundImage = "url('images/defaultPL.png')";
+    }
+
+    img.setAttribute("class", "records");
+    var info=document.createElement("div");
+    info.setAttribute("class", "info");
+    var text=document.createElement("p");
+    text.innerHTML = ("<h3>Izvajalec: Devil's gun</h3><h2>Naslov: Raising The Beast</h2>");
+
+    info.appendChild(text);
+
+    
+    
+    document.getElementById("main").appendChild(img);
+    img.appendChild(info);
+
+  }
+	
+});
+ 
 function expand() {
 	
 	if(document.getElementById("aside").style.zIndex == "" || document.getElementById("aside").style.zIndex == "1"){
@@ -14,3 +49,4 @@ function expand() {
 		document.getElementById("aside").style.zIndex = "1";
 	}
 }
+
